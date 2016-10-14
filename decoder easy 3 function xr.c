7ab8b915bc04dd78f43a5f5dc3770e22207db4b8
ankit16-19 <ankitsocial16@gmail.com>
2016-10-14 19:53:51 +0530

    Create decoder easy 3 function xr.c

---
+++ decoder easy 3 function xr.c
@@ -0,0 +1,11 @@
+//to determine a function fx
+#include<stdio.h>
+main()
+{
+    printf("Enter a integer to get its functional value:\n");
+    int num;
+    scanf("%d",&num);
+    int x;
+    x = num%7;
+    printf("The functional value of x is:\n%d",x);
+}
